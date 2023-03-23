8733a2bd09f0ca49c9eb6ec7763197f06950b7a3
Josue Benjamin <josuebcenturion@gmail.com>
2023-03-23 15:40:19 -0300

    Simple Frontend, Not added Auth0 yet

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
+// IMPORTANT INFO: Swagger tags were added. 
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  res.render('home',  { user: req.user });
 });
+
+// router.get('/', (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+// });
+
 router.use('/api-docs', require('./swagger'));
 router.use('/courses', require('./courses'));
 router.use('/instructors', require('./instructors'));
 router.use('/profile', require('./profile'));
+router.use('/auth', require('./auth'));
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  req.oidc.user ? res.send(JSON.stringify(req.oidc.user)) : res.redirect('/login');
+  req.oidc.user ? res.send(JSON.stringify(req.oidc.user)) : res.redirect('/auth/login');
 });
+
+// Code to check if the user is already login, it's the same purpose than line 4 code.
+// const authCheck = (req, res, next) => {
+//   if(!req.user){
+//       res.redirect('/auth/login');
+//   } else {
+//       next();
+//   }
+// };
+
+// router.get('/', authCheck, (req, res) => {
+//   res.render('profile', { user: req.user });
+// });
+
 
 module.exports = router;
