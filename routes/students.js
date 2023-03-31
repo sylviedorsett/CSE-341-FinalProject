69cead47eb92e5407516864d969691ccab140fa6
Santiago Pintus <santiagopintus@gmail.com>
2023-03-31 14:19:25 -0300

    Removed auth.isAuthenticated from all POST and PUT routes

--- routes/students.js
+++ routes/students.js
@@ -2,12 +2,11 @@ const router = require('express').Router();
 
 const getStudentsController = require('../controllers/students');
 const validate = require('../middleware/validate');
-const auth = require('../middleware/authenticate.js');
 
 router.get('/', getStudentsController.getAllStudents);
 router.get('/:id', getStudentsController.getStudent);
-router.post('/', auth.isAuthenticated, validate.newStudent, getStudentsController.postStudent);
-router.put('/:id', auth.isAuthenticated, validate.newStudent, getStudentsController.putStudent);
+router.post('/', validate.newStudent, getStudentsController.postStudent);
+router.put('/:id', validate.newStudent, getStudentsController.putStudent);
 router.delete('/:id', getStudentsController.deleteStudent);
 
 module.exports = router;
